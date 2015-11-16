0429af84a7c0a790dd5ee6d1633f5812e4b843b1
chtmorris <agoosester@gmail.com>
2015-11-16 18:21:25 +0800

    Added collection views

---
+++ CostToMakeAnApp/AnswerCardCell.swift
@@ -0,0 +1,21 @@
+//
+//  AnswerCardCell.swift
+//  CostToMakeAnApp
+//
+//  Created by Charlie Morris on 16/11/2015.
+//  Copyright © 2015 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCardCell: UICollectionViewCell {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cardImage: UIImageView!
+    
+    func configure(description: String?, image:UIImage?) {
+        descriptionLabel.text = description
+        cardImage.image = image
+    }
+    
+}
--- CostToMakeAnApp/ViewController.swift
+++ CostToMakeAnApp/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let descriptions = ["Android", "Apple iOS", "Android and Apple iOS"]
+    let images = [UIImage(named: "Android"), UIImage(named: "iOS"), UIImage(named: "iOS and Android")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return descriptions.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("answerCard", forIndexPath: indexPath)
+        
+        if let cell = cell as? AnswerCardCell {
+            let description = descriptions[indexPath.row]
+            let image = images[indexPath.row]
+            cell.configure(description, image: image)
+        }
+        
+        
+        return cell
+    
+    }
 
 }
 
