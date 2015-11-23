81c45d4a7863549f487a5ef7d47e2d47a47e41bb
chtmorris <agoosester@gmail.com>
2015-11-23 13:56:22 +0800

    Added OptionsManager

---
+++ CostToMakeAnApp/OptionsManager.swift
@@ -0,0 +1,34 @@
+//
+//  PriceCalculator.swift
+//  CostToMakeAnApp
+//
+//  Created by Charlie Morris on 23/11/2015.
+//  Copyright © 2015 Mind Fund Studio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OptionsManager {
+    
+    static let sharedInstance = OptionsManager()
+    var answerArray = Array<Int>()
+    var totalPrice = Int()
+    var totalCostLabel: UILabel!
+    
+    init() {
+        print("Created OptionsManager instance")
+    }
+    
+    func addToPrice(itemToBeAdded:Int) {
+        answerArray.append(itemToBeAdded)
+        totalPrice = answerArray.count * 5000
+        totalCostLabel.text = "$\(totalPrice)"
+    }
+    
+    func startAgainButtonTapped() {
+        answerArray = []
+        totalCostLabel.text = "$0"
+    }
+    
+}
