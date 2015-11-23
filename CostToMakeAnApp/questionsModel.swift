c1507a224eb80a4f7a58c37dc3d6d19d9d6c3c3a
chtmorris <agoosester@gmail.com>
2015-11-23 10:57:32 +0800

    Added remaining questions

--- CostToMakeAnApp/questionsModel.swift
+++ CostToMakeAnApp/questionsModel.swift
@@ -22,8 +22,14 @@ enum Questions:Int {
             return "Do people create profiles?"
         case .MoneyMaking:
             return "How will you make money?"
-        default:
-            return "Lala"
+        case .RateOrReview:
+            return "Do people rate or review things?"
+        case .API:
+            return "Does your app need to connect with a website?"
+        case .Design:
+            return "How nice should your app look?"
+        case .Icon:
+            return "Do you need an app icon?"
         }
     }
     
@@ -37,8 +43,14 @@ enum Questions:Int {
             return ["Yes", "None", "I don't know"]
         case .MoneyMaking:
             return ["Upfront cost", "In-app purchases", "I won't", "I don't know"]
-        default:
-            return ["Lala"]
+        case .RateOrReview:
+            return ["Yes", "No", "I don't know"]
+        case .API:
+            return ["Yes", "No", "I don't know"]
+        case .Design:
+            return ["Bare-bones", "Stock", "Beautiful"]
+        case .Icon:
+            return ["Yes", "No", "I don't know"]
         }
     }
     
@@ -53,8 +65,14 @@ enum Questions:Int {
             return [UIImage(named: "Profile")!, UIImage(named: "None")!, UIImage(named: "?")!]
         case .MoneyMaking:
             return [UIImage(named: "UpfrontCost")!,UIImage(named: "InAppPurchase")!, UIImage(named: "None")!, UIImage(named: "?")!]
-        default:
-            return [UIImage(named: "Profile")!, UIImage(named: "None")!, UIImage(named: "?")!]
+        case .RateOrReview:
+            return [UIImage(named: "Rate")!, UIImage(named: "None")!, UIImage(named: "?")!]
+        case .API:
+            return [UIImage(named: "ConnectWebsite")!, UIImage(named: "None")!, UIImage(named: "?")!]
+        case .Design:
+            return [UIImage(named: "BareBones")!, UIImage(named: "Stock")!, UIImage(named: "Beautiful")!]
+        case .Icon:
+            return [UIImage(named: "AppIcon")!, UIImage(named: "None")!, UIImage(named: "?")!]
         }
     }
     
@@ -69,15 +87,17 @@ enum Questions:Int {
             return "A profile means that some of the people that use your app will have to enter information about themselves that might be viewed publicly."
         case .MoneyMaking:
             return "Upfront cost is cheaper to build than in-app purchases but in-app purchase can produce higher returns if/when you have an engaged user base."
-        default:
-            return "lala"
-            
-//            "Keep in mind, ranking systems can vary in complexity and thus impact your budget.",
-//            "This means you'll need to make an API (or Application Programming Interface.) It's how all your friendly apps talk to each other.",
-//            "Custom gestures, custom buttons, and custom transitions will cost you — it's not cheap being pretty.",
-//            "Designing a quality app icon typically ranges from $500 - $2,000 but will help you stand out in the app store and on the device."
-
+        case .RateOrReview:
+            return "Keep in mind, ranking systems can vary in complexity and thus impact your budget."
+        case .API:
+            return "This means you'll need to make an API (or Application Programming Interface.) It's how all your friendly apps talk to each other."
+        case .Design:
+            return "Custom gestures, custom buttons, and custom transitions will cost you — it's not cheap being pretty."
+        case .Icon:
+            return "Designing a quality app icon typically ranges from $500 - $2,000 but will help you stand out in the app store and on the device."
         }
     }
+    
+    static var count: Int { return Questions.Icon.hashValue + 1}
 
 }
