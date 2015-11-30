3ef2c2eaaf9b558793bc7a539fb709ab27ca7235
chtmorris <agoosester@gmail.com>
2015-11-30 17:19:24 +0800

    Added project summary VC

---
+++ CostToMakeAnApp/ProjectSummaryCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectSummaryCell.swift
+//  CostToMakeAnApp
+//
+//  Created by Charlie Morris on 24/11/2015.
+//  Copyright © 2015 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ProjectSummaryCell: UICollectionViewCell {
+    
+}
