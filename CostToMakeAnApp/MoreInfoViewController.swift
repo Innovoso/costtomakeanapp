bd51d186547d8284bdc310375022c2393c75f7f4
chtmorris <agoosester@gmail.com>
2015-12-01 14:07:44 +0800

    Added touch controls to more info card

--- CostToMakeAnApp/MoreInfoViewController.swift
+++ CostToMakeAnApp/MoreInfoViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class MoreInfoViewController: UIViewController {
 
+    @IBOutlet weak var moreInfoCard: UIView!
+    
+    var screenSize: CGRect = UIScreen.mainScreen().bounds
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+
+        view.userInteractionEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +28,46 @@ class MoreInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    // =============================
+    // MORE INFO CARD TOUCH CONTROLS
+    // =============================
+    
+    @IBAction func moreInfoButtonPressed(sender: UIButton) {
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            self.moreInfoCard.center = CGPoint(x: self.screenSize.width/2, y: self.screenSize.height - 200)
+//            self.screenOverlay.alpha = 0.5
+            
+            }, completion: nil)
+        
     }
-    */
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in (touches) {
+            let location = touch.locationInView(self.view)
+            
+            if moreInfoCard.frame.contains(location) {
+                moreInfoCard.center = location
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch in (touches) {
+            let location = touch.locationInView(self.view)
+            
+            if moreInfoCard.frame.contains(location) {
+                moreInfoCard.center = location
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveLinear, animations: { () -> Void in
+            self.moreInfoCard.center = CGPoint(x: self.screenSize.width/2, y: self.screenSize.height + 60)
+//            self.screenOverlay.alpha = 0
+            }, completion: nil)
+    }
 
 }
