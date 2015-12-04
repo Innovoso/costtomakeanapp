13b05df62d8332b953ba957409a7868fc3c94391
chtmorris <agoosester@gmail.com>
2015-12-04 11:58:33 +0800

    Added scroll view to contact view controller

--- CostToMakeAnApp/CMContactViewController.swift
+++ CostToMakeAnApp/CMContactViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class CMContactViewController: UIViewController {
-
+    
+    @IBOutlet weak var contentView: UIView!
+    var scrollView: UIScrollView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        contentView.frame.size.width = view.frame.width
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor(red: 219/255, green: 51/255, blue: 64/255, alpha: 1)
+        scrollView.contentSize = contentView.bounds.size
+        scrollView.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+        
+        scrollView.addSubview(contentView)
+        view.addSubview(scrollView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
     
 
