de5cf44328c6020448b4a28f21008680a1a5a5d5
chtmorris <agoosester@gmail.com>
2015-12-16 10:19:45 +0800

    Removed navigation bar from launch page

---
+++ LaunchPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LaunchPageViewController.swift
+//  CostToMakeAnApp
+//
+//  Created by Charlie Morris on 16/12/2015.
+//  Copyright © 2015 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class LaunchPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
